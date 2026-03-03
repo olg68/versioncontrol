6b050326ac4bab78c655d5c0077f553a53646670
Yup777 <yup52@pitt.edu>
2026-03-03 17:26:15 -0500

    Update nameandnumberinput.py

--- nameandnumberinput.py
+++ nameandnumberinput.py
@@ -1,5 +1,6 @@
 name = input("Enter your name: ")
 times = int(input("Enter a number: "))
+age = int(input("Enter your age: "))
 
-print("I am going to say your name " + str(times) + " times")
-print((name + " ") * times)
+print("I am going to say your name and age " + str(times) + " times")
+print((name + age + " ") * times)
